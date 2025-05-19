ea1877dded8b2fbc4a4b833d1cf311342790649c
César Jerez <csrstrps@gmail.com>
2025-05-19 10:26:49 -0600

    Update .gitignore to exclude build artifacts

---
+++ src/app/experimental/open-cascade-studio/page.tsx
@@ -0,0 +1,7 @@
+export default function CascadePage() {
+  return (
+    <div className="h-screen">
+      <iframe src="/cascade-studio/index.html" className="w-full h-full border-none" />
+    </div>
+  )
+}
